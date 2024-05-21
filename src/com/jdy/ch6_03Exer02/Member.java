1797fbda2764df4f8009d36db50d4d801f238a94
wjseoduq3 <dragon65@nate.com>
2024-05-21 21:36:18 +0900

    today last commit

---
+++ src/com/jdy/ch6_03Exer02/Member.java
@@ -0,0 +1,11 @@
+package com.jdy.ch6_03Exer02;
+
+public class Member {
+	String name;
+	String id;
+	
+	public Member(String name, String id) {
+		this.name = name;
+		this.id = id;		
+	}
+}
---
+++ src/com/jdy/ch6_03Exer02/MemberExample.java
@@ -0,0 +1,12 @@
+package com.jdy.ch6_03Exer02;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Member member1 = new Member("홍길동", "hong");
+		System.out.println(member1.name);
+		System.out.println(member1.id);
+	}
+}
